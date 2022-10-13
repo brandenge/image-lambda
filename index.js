0fc7e6fe3bb02e8992878d25417f3804ed135aff
Branden Ge <86636108+brandenge@users.noreply.github.com>
2022-10-12 20:21:20 -0500

    Lab 17 complete

--- index.js
+++ index.js
@@ -3,16 +3,32 @@ const S3 = new AWS.S3();
 
 exports.handler = async (event) => {
   const bucketName = event.Records[0].s3.bucket.name;
-  const key = event.Records[0].s3.object.key;
+  const fileName = event.Records[0].s3.object.key;
   const size = event.Records[0].s3.object.size;
+  let parsedImages = [];
+  try {
+    const responseObject = await S3.getObject({Bucket: bucketName, Key: 'images.json'}).promise();
+    const stringifiedImages = responseObject.Body.toString();
+    parsedImages = JSON.parse(stringifiedImages);
+  } catch (e) {
+    console.log('ERROR: the file does not exist yet - it will be created', e);
+  }
 
-  const responseObject = await S3.getObject({Bucket: bucketName, Key: key}).promise();
-  const stringifiedImages = JSON.stringify(responseObject.Body);
-  const parsedImages = JSON.parse(stringifiedImages);
-
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify(parsedImages),
+  const newImage = {
+    name: fileName,
+    size: size,
+    type: 'jpg',
   };
-  return response;
+
+  const foundIndex = parsedImages.findIndex(image => image.name === fileName);
+  if (foundIndex === -1) parsedImages.push(newImage);
+  else parsedImages[foundIndex] = newImage;
+
+  const params = {
+    Bucket: bucketName,
+    Key: 'images.json',
+    Body: JSON.stringify(parsedImages),
+    ContentType: 'application/json',
+  };
+  await S3.putObject(params).promise();
 };
